c91850feaa56872718483ba4192b3cea214d47f4
Joaovitorleao <97983498+Joaovitorleao@users.noreply.github.com>
2023-11-27 17:10:37 -0300

    feat: some initial codes added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const routes = require("./routes");
 
 require("dotenv").config();
 
@@ -7,5 +8,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(routes);
+app.use(express.urlencoded({ extended: true }));
+app.use("*", (req, res) => {
+    res.status(404).json({ message: `Rota '${req.baseUrl}' não encontrada.` });
+});
 
 module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const app = require("./App");
 const Loaders = require("./Loaders/index");
+const dotenv = require("dotenv");
 
 Loaders.start();
+dotenv.config();
 
-app.listen(process.env.PORT, () => console.log("Servidor Rodando!"));
+app.listen(process.env.PORT || 8000, () => console.log("Servidor Rodando!"));
